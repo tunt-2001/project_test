0fafec890bd32a19644b5b877e8b4e38967c2028
Dell E7250 <trongtu28032001@gmail.com>
2023-03-23 10:13:53 +0700

    ssf

--- lib/main.dart
+++ lib/main.dart
@@ -21,22 +21,41 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  String searchText ='';
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Contact> contacts = [];
+  final List<Contact> contacts = [
+    Contact(
+      name: 'John Doe',
+      phone: '5556661234',
+      avatar: 'https://ap.poly.edu.vn/images/logo.png',
+    ),
+    Contact(
+      name: 'Jane Doe',
+      phone: '5556665678',
+      avatar: 'https://ap.poly.edu.vn/images/logo.png',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Contacts'),
+        // actions: [
+        //   IconButton(onPressed: () => setState(() {
+        //
+        //   }), icon: Icon(Icons.search,size: 30,),
+        //   )
+        // ],
         actions: [
-         IconButton(onPressed: (){}, icon: Icon(Icons.search),
-          )
+          // Navigate to the Search Screen
+          IconButton(
+              onPressed: () => Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (_) => const SearchPage())),
+              icon: const Icon(Icons.search))
         ],
       ),
       body: ListView.builder(
@@ -50,17 +69,20 @@ class _MyHomePageState extends State<MyHomePage> {
                 backgroundImage: NetworkImage(contact.avatar),
                 backgroundColor: Colors.white,
               ),
-              title: Text(contact.name,
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 20,
-
-              ),),
-              subtitle: Text(contact.phone,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 15,
-              ),),
+              title: Text(
+                contact.name,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 20,
+                ),
+              ),
+              subtitle: Text(
+                contact.phone,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
             ),
             secondaryActions: [
               IconSlideAction(
@@ -97,7 +119,6 @@ class _MyHomePageState extends State<MyHomePage> {
           );
         },
       ),
-
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           final newContact = await Navigator.push(
@@ -115,6 +136,38 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Add Contact',
         child: Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          // The search area here
+          title: Container(
+        width: double.infinity,
+        height: 40,
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(5)),
+        child: Center(
+          child: TextField(
+            decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.clear),
+                  onPressed: () {
+                    /* Clear the search field */
+                  },
+                ),
+                hintText: 'Search...',
+                border: InputBorder.none),
+          ),
+        ),
+      )),
     );
   }
 }
